249b20c4f3dc56570b3a2a98c5f63c6a91868985
Ali Assaf <ali.assaf@inria.fr>
2013-06-26 21:03:54 +0200

    Add a testing file.

---
+++ src/test.ml
@@ -0,0 +1,7 @@
+let a = Type.type_app "pair" [Type.type_var "A"; Type.type_app "list" [Type.type_var "B"]]
+
+let a' = Type.translate_type a
+
+let _ =
+  Printf.printf "Testing...\n";
+  Printf.printf "%a\n" Dedukti.print_term a'
