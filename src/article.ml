e8fa76b098953ddeee05a3cd86667d66cbb59405
Ali Assaf <ali.assaf@inria.fr>
2013-06-28 17:16:25 +0200

    Add the HOL theorems to the virtual machine. Can translate theorems, the others are left as axioms for now.

--- src/article.ml
+++ src/article.ml
@@ -4,6 +4,7 @@
     This code is inspired by the import code of HOL Light:
     http://src.gilith.com/hol-light.html *)
 
+type thm = Thm.thm
 
 (** The objects and stack of the virtual machine. *)
 type stack_object =
@@ -15,7 +16,7 @@ type stack_object =
 | Const of string
 | Var of string * Type.hol_type
 | Term of Term.term
-| Thm
+| Thm of Thm.thm
 
 type stack = stack_object list
 
@@ -57,36 +58,38 @@ let process_command cmd stack =
   if c = '#' then stack else (* Ignore comments *)
   if c = '\"' then process_name stack cmd else
   if Name.is_numerical c then process_num stack cmd else
+(*  let _ = Output.print_verbose "Processing %s\n%!" cmd in*)
+(*  let _ = Output.print_comment cmd in*)
   match cmd, stack with
   | "absTerm", Term(t) :: Var(x, a) :: stack -> Term(Term.lam x a t) :: stack
-  | "absThm", Thm :: Var(x, a) :: stack -> Thm :: stack
+  | "absThm", Thm(thm_tu) :: Var(x, a) :: stack -> Thm(Thm.abs_thm x a thm_tu) :: stack
   | "appTerm", Term(u) :: Term(t) :: stack -> Term(Term.app t u) :: stack
-  | "appThm", Thm :: Thm :: stack -> Thm :: stack
-  | "assume", Term(p) :: stack -> Thm :: stack
+  | "appThm", Thm(thm_tu) :: Thm(thm_fg) :: stack -> Thm(Thm.app_thm thm_fg thm_tu) :: stack
+  | "assume", Term(p) :: stack -> Thm(Thm.assume p) :: stack
   | "axiom", Term(p) :: List(gamma) :: stack ->
       let extract_term obj =
         match obj with
         | Term(t) -> t
         | _ -> failwith "not a term object" in
-      let _ = List.map extract_term gamma in
-      Thm :: stack
-  | "betaConv", Term(Term.App(Term.Lam(x, a, t), u)) :: stack -> Thm :: stack
+      let gamma = List.map extract_term gamma in
+      Thm(Thm.axiom gamma p) :: stack
+  | "betaConv", Term(Term.App(Term.Lam(x, a, t), u)) :: stack -> Thm(Thm.beta_conv x a t u) :: stack
   | "cons", List(tail) :: head :: stack -> List(head :: tail) :: stack
   | "const", Name(name) :: stack -> Const(name) :: stack
   | "constTerm", Type(a) :: Const(c) :: stack -> Term(Term.cst c a) :: stack
-  | "deductAntisym", Thm :: Thm :: stack -> Thm :: stack
+  | "deductAntisym", Thm(thm_q) :: Thm(thm_p) :: stack -> Thm(Thm.deduct_anti_sym thm_p thm_q) :: stack
   | "def", Num(k) :: obj :: stack ->
       dict_add k obj;
       obj :: stack
-  | "defineConst", Term(t) :: Name(n) :: stack -> Thm :: Const(n) :: stack
-  | "defineTypeOp", Thm :: List(type_vars) :: Name(rep_name) :: Name(abs_name) :: Name(op_name) :: stack ->
-      let extract_name obj =
-        match obj with
-        | Name(n) -> n
-        | _ -> failwith "not a name object" in
-      let _ = List.map extract_name type_vars in
-      Thm :: Thm :: Const(rep_name) :: Const(abs_name) :: TypeOp(op_name) :: stack
-  | "eqMp", Thm :: Thm :: stack -> Thm :: stack
+  | "defineConst", Term(t) :: Name(n) :: stack -> Thm(Thm.define_const n t) :: Const(n) :: stack
+(*  | "defineTypeOp", Thm :: List(type_vars) :: Name(rep_name) :: Name(abs_name) :: Name(op_name) :: stack ->*)
+(*      let extract_name obj =*)
+(*        match obj with*)
+(*        | Name(n) -> n*)
+(*        | _ -> failwith "not a name object" in*)
+(*      let _ = List.map extract_name type_vars in*)
+(*      Thm :: Thm :: Const(rep_name) :: Const(abs_name) :: TypeOp(op_name) :: stack*)
+  | "eqMp", Thm(thm_p) :: Thm(thm_pq) :: stack -> Thm(Thm.eq_mp thm_p thm_pq) :: stack
   | "nil", stack -> List([]) :: stack
   | "opType", List(args) :: TypeOp(type_op) :: stack ->
       let extract_type obj =
@@ -97,12 +100,12 @@ let process_command cmd stack =
       Type(Type.app type_op args) :: stack
   | "pop", _ :: stack -> stack
   | "ref", Num(k) :: stack -> dict_find k :: stack
-  | "refl", Term(t) :: stack -> Thm :: stack
+  | "refl", Term(t) :: stack -> Thm(Thm.refl t) :: stack
   | "remove", Num(k) :: stack ->
       let obj = dict_find k in
       Hashtbl.remove dict k;
       obj :: stack
-  | "subst", Thm :: List([List(theta); List(sigma)]) :: stack ->
+  | "subst", Thm(thm) :: List([List(theta); List(sigma)]) :: stack ->
       let extract_type_subst obj =
         match obj with
         | List([Name(x); Type(a)]) -> (x, a)
@@ -111,15 +114,16 @@ let process_command cmd stack =
         match obj with
         | List([Var(x, a); Term(t)]) -> ((x, a), t)
         | _ -> failwith "not a term substitution" in
-      let _ = List.map extract_type_subst theta in
-      let _ = List.map extract_term_subst sigma in
-      Thm :: stack
-  | "thm", Term(p) :: List(qs) :: Thm :: stack ->
+      let theta = List.map extract_type_subst theta in
+      let sigma = List.map extract_term_subst sigma in
+      Thm(Thm.subst sigma (Thm.type_subst theta thm)) :: stack
+  | "thm", Term(p) :: List(qs) :: Thm(thm) :: stack ->
       let extract_term obj =
         match obj with
         | Term(t) -> t
         | _ -> failwith "not a term" in
       let _ = List.map extract_term qs in
+      let _ = Thm.thm thm in
       stack
   | "typeOp", Name(op) :: stack -> TypeOp(op) :: stack
   | "var", Type(a) :: Name(x) :: stack -> Var(x, a) :: stack
