7f00d29eda6c184bd4000941cde083f6eb7fb984
schnappi <schnappi@zero.home>
2019-09-24 15:22:15 +0200

    Differentiate sorting and database, start work to construct iteratively constructed database

--- src/database.ml
+++ src/database.ml
@@ -1,47 +1,36 @@
 (** Creating and using the database of articles inputs and outputs *)
 
-open Graph
+(** Building the database incrementally *)
 
-module Dep =
-  struct
-  
-	module Str = struct
-		type t = string
-		let compare = String.compare
-		let hash = Hashtbl.hash
-		let equal = String.equal
-	end
-	
-	include Imperative.Digraph.Concrete(Str)
-  end
+(* See if the database exists *)
 
-let dep = Dep.create()
+let file_exists = Sys.file_exists
 
-module OrderedDep  = Topological.Make(Dep)
+let rec find_database () =
+  if file_exists "database.holide"
+    then open_in "database.holide"
+  else failwith "Database not found"
 
-(* Adding a file to the graph *)
+(* Construct the database *)
 
-let add_mod mod_name =
-	Dep.add_vertex dep mod_name
+let marshal defined_typeops defined_csts outputs =
+  try
+    let file = "database.holide" in
+    let oc = open_out file in
+    Marshal.to_channel oc defined_typeops [];
+    Marshal.to_channel oc defined_csts [];
+    Marshal.to_channel oc outputs [];
+    close_out oc; true
+  with _ -> false
 
-(* Adding dependencies *)
+(* Import from database *)
 
-let add_dep mod_name dep_name =
-	Dep.add_edge dep dep_name mod_name
-
-let fill_dep l =
-	List.iter add_mod l;
-	Hashtbl.iter add_dep (Type.deps)
-
-(* Find dependencies of a given module *)
-
-let dependencies mod_name =
-	Dep.pred dep mod_name
-
-(* Find order *)
-
-let ordereddep f g = OrderedDep.iter f g
-
-(* Number of added files *)
-
-let number_dep () = Dep.nb_vertex dep
+let unmarshal () =
+  try
+    let chan = find_database () in
+    let tdops		= Marshal.from_channel chan in
+    let tdcsts		= Marshal.from_channel chan in
+    let toutputs	= Marshal.from_channel chan in
+    close_in chan; Some (tdops,tdcsts,toutputs)
+  with
+  | _ -> None
