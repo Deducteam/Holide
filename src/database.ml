cb62a5ad27afcb5bb834b932d48e8ba4ed3bb1cf
Emilie Grienenberger <emilie.grienenberger@lsv.fr>
2019-11-04 14:22:33 +0100

    work on holide object database

--- src/database.ml
+++ src/database.ml
@@ -6,31 +6,34 @@
 
 let file_exists = Sys.file_exists
 
-let rec find_database () =
-  if file_exists "database.holide"
-    then open_in "database.holide"
+let rec find_database database =
+  if file_exists database
+    then open_in database
   else failwith "Database not found"
 
 (* Construct the database *)
 
-let marshal defined_typeops defined_csts outputs =
+let marshal content file =
   try
-    let file = "database.holide" in
     let oc = open_out file in
-    Marshal.to_channel oc defined_typeops [];
-    Marshal.to_channel oc defined_csts [];
-    Marshal.to_channel oc outputs [];
+    Marshal.to_channel oc content [];
     close_out oc; true
   with _ -> false
 
 (* Import from database *)
 
-let unmarshal () =
+let unmarshal_h database =
   try
-    let chan = find_database () in
-    let tdops		= Marshal.from_channel chan in
-    let tdcsts		= Marshal.from_channel chan in
-    let toutputs	= Marshal.from_channel chan in
-    close_in chan; Some (tdops,tdcsts,toutputs)
+    let chan = find_database database in
+    let content   = Marshal.from_channel chan in
+    close_in chan; content
   with
-  | _ -> None
+  | _ -> Hashtbl.create 100
+
+let unmarshal_l database =
+  try
+    let chan = find_database database in
+    let content   = Marshal.from_channel chan in
+    close_in chan; content
+  with
+  | _ -> []
